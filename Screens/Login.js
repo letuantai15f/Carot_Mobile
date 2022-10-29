6fa813c73caee518c24c022fc44544a207b7315f
TaiLee <letuantai15f@gmail.com>
2022-10-29 17:04:18 +0700

    Tai_login

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,96 +1,152 @@
-import React  , {useState } from 'react';
-import {View, StyleSheet,Text ,TouchableOpacity,TextInput} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { firebase } from '../config/firebaseSDK';
-import SignUp from '../Screens/SignUp';
+import Signup from '../Screens/SignUp';
 import AppNavigator from '../navigation/AppNavigator';
+import imgcarrot from '../assets/carrot.png'
+import { useFonts } from 'expo-font'
+import imgGG from '../assets/google-logo-9824.png'
 
 
-const Login = ({navigation}) => {
+const Login = ({ navigation }) => {
   // const navigation = useNavigation()   
-  
-  const [email,setEmail] = useState(' ')
-  const [password,setPassword] = useState(' ')
-   
 
-  const loginUser = async(email,password) =>{
+  const [email, setEmail] = useState(' ')
+  const [password, setPassword] = useState(' ')
+
+
+  const loginUser = async (email, password) => {
     try {
-      await firebase.auth().signInWithEmailAndPassword(email,password) 
-            
-        .then(()=>navigation.navigate(AppNavigator))      
-    } catch (error){
+
+      await firebase.auth().signInWithEmailAndPassword(email, password)
+
+        .then(() => navigation.navigate(AppNavigator))
+      console.log('dang nhap thanh cong')
+    } catch (error) {
       alert('Tài khoản chưa được đăng kí')
     }
-  } 
-  
+  }
+
   return (
     <View style={styles.container}>
-        {/* <Text style={{fontWeight:'bold',fontSize:26}}>Login</Text> */}
-        <View style={{marginTop:40}}>
-             <TextInput
-              style={styles.textInput}
-              placeholder="Email"
-              onChangeText={(email)=>setEmail(email)}   
-              autoCapitalize="none"
-              autoCorrect={false}
-              />
 
-              <TextInput
-              style={styles.textInput}
-              placeholder="Password"
-              onChangeText={(password)=>setPassword(password)}   
-              autoCapitalize="none"
-              autoCorrect={false}
-              />
+      <View style={styles.navtop}>
+
+        <Text style={styles.textlogin}>CAROT</Text>
+        <Image style={styles.img} source={imgcarrot} />
+
+      </View>
+
+      <View style={styles.navbody}>
+        <View style={{  }}>
+          <Text>Email:</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email"
+            onChangeText={(email) => setEmail(email)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+           <Text>Mat Khau:</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Mật khẩu"
+            onChangeText={(password) => setPassword(password)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
         </View>
+        <View style={styles.navbottom}>
+          <TouchableOpacity
+            onPress={() => loginUser(email, password)}
+            style={styles.button}>
+            <Text style={{ fontWeight: "bold", fontSize: 21,color:'white' }}>Đăng nhập</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-        onPress={()=>loginUser(email,password)}
-        style={styles.button}>
-          <Text style={{fontWeight:"bold",fontSize:21}}>Login</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate({ name: 'SignUp' })}
+            style={{ marginTop: 20 }}>
+            <Text style={{  fontSize: 16 }}>
+              Bạn chưa có tài khoản? Đăng ký ngay
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
 
-        <TouchableOpacity
-           onPress={()=>navigation.navigate(SignUp)}
-           style={{marginTop:20}}>
-             <Text style={{fontWeight:"bold",fontSize:16}}>   
-              Don't have an accout?Registry Now
-             </Text>
-        </TouchableOpacity>           
+      <View style={{ flex: 1, }}>
+
+
+      </View>
+
+
+
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-     container : {
-      flex:1,
-      alignItems:'center',
-      marginTop:100,
-     },
-     textInput:{
-      paddingTop:20,
-      paddingBottom:10,
-      width:400,
-      fontSize:20,
-      borderBottomWidth:1,
-      borderBottomColor:'#000',
-      marginBottom:10,
-      textAlign:'center'
-     },
-     button :{
-      marginTop:50,
-      height:70,
-      width:250,
-      backgroundColor:'#026efd',
-      alignItems:'center',
-      justifyContent:'center',
-      borderRadius:50
-     }
+  container: {
+    flex: 1,
+    marginTop: 40,
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: '#ff4c00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50
+  },
+  navtop: {
+    alignItems: 'center',
+    flex: 1
+  },
+  navbody: {
+    marginTop: 10,
+    flex: 2, alignItems: 'center'
+
+
+  },
+  navbottom: {
+    flex: 1,
+    alignItems: 'center',
+
+  },
+  img: {
+    height: 130,
+    width: 130,
+    rotation: 30,
+    marginTop: -20,
+    marginBottom: -50
+
+  },
+  textlogin: {
+    fontSize: 70,
+    color: 'red',
+    fontWeight: 'bold'
+  },
+  textnoti: {
+    fontSize: 20,
+  },
+  textInput: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    textAlign: 'center',
+    flexDirection: 'row',
+    borderColor: '#ff4c00',
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    width: 300,
+    justifyContent: 'space-around',
+    marginTop: 20
+  },
 })
 
 export default Login;
-
-
-
-
